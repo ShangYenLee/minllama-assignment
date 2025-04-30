f17d8dfee6dcebc52351bfdf4316b670f7f080de
ShangYenLee <sun410068@gmail.com>
2025-04-30 10:52:16 +0800

    Update optimizer.py and passed optimizer_test.py

--- optimizer.py
+++ optimizer.py
@@ -1,5 +1,5 @@
 from typing import Callable, Iterable, Tuple
-
+import math
 import torch
 from torch.optim import Optimizer
 
@@ -38,23 +38,43 @@ class AdamW(Optimizer):
                 if grad.is_sparse:
                     raise RuntimeError("Adam does not support sparse gradients, please consider SparseAdam instead")
 
-                raise NotImplementedError()
+                # raise NotImplementedError()
 
                 # State should be stored in this dictionary
                 state = self.state[p]
 
                 # Access hyperparameters from the `group` dictionary
                 alpha = group["lr"]
+                beta1, beta2 = group["betas"]
+
+                # State Initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    state['exp_avg_sq'] = torch.zeros_like(p.data)
 
                 # Update first and second moments of the gradients
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                state['step'] += 1
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
 
                 # Bias correction
                 # Please note that we are using the "efficient version" given in
                 # https://arxiv.org/abs/1412.6980
-
+                bias_correction1 = 1 - beta1 ** state['step']
+                bias_correction2 = 1 - beta2 ** state['step']
+                denom = (exp_avg_sq.sqrt()/math.sqrt(bias_correction2)).add_(group["eps"])
+                if group["correct_bias"]:
+                    step_size = alpha / bias_correction1
+                else:
+                    step_size = alpha
+                
                 # Update parameters
-
+                p.data.addcdiv_(exp_avg, denom, value=-step_size)
                 # Add weight decay after the main gradient-based updates.
                 # Please note that the learning rate should be incorporated into this update.
+                if abs(group["weight_decay"]) > 1e-9:
+                    p.data.add_(p.data, alpha=-step_size * group["weight_decay"])
 
         return loss
